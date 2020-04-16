b3810187ea5112677ee29eaf0fabe88b6339e08d
Ian French <igf2011@hotmail.com>
2020-04-15 23:30:52 -0400

    Completed guided project

--- HobbyTracker/View Controllers/AddFriendViewController.swift
+++ HobbyTracker/View Controllers/AddFriendViewController.swift
@@ -8,6 +8,18 @@
 
 import UIKit
 
+// delegator critical steps
+// step 1, create  delegate protocol
+// step 2 create delegate proprty on delegator file of type delegate
+// step 3 call methods on delegate when needed
+
+
+// delegate
+protocol AddFriendDelegate {
+    func newFriendAdded(friend: Friend)
+}
+
+
 class AddFriendViewController: UIViewController {
 
     @IBOutlet weak var nameTextField: UITextField!
@@ -16,11 +28,46 @@ class AddFriendViewController: UIViewController {
     @IBOutlet weak var hobby2TextField: UITextField!
     @IBOutlet weak var hobby3TextField: UITextField!
     
+    var delegate: AddFriendDelegate?
+    
+    
     @IBAction func cancelTapped(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
     }
     
     @IBAction func saveTapped(_ sender: UIBarButtonItem) {
+        
+        guard let name = nameTextField.text,
+        let hometown = hometownTextField.text,
+            !name.isEmpty,
+            !hometown.isEmpty else { return}
+        
+        var friend = Friend(name: name, hometown: hometown, hobbies: [])
+        
+        
+        
+        if let hobby1 = hobby1TextField.text,
+            !hobby1.isEmpty  {
+            friend.hobbies.append(hobby1)
+        }
+        
+        if let hobby2 = hobby2TextField.text,
+                  !hobby2.isEmpty  {
+                  friend.hobbies.append(hobby2)
+        
+        }
+        
+        if let hobby3 = hobby3TextField.text,
+                  !hobby3.isEmpty  {
+                  friend.hobbies.append(hobby3)
+       }
+        
+        delegate?.newFriendAdded(friend: friend)
+        dismiss(animated: true, completion: nil)
+        
+        
     }
+        
 }
 
 extension AddFriendViewController: UITextFieldDelegate {
--- HobbyTracker/View Controllers/FriendsTableViewController.swift
+++ HobbyTracker/View Controllers/FriendsTableViewController.swift
@@ -10,22 +10,62 @@ import UIKit
 
 class FriendsTableViewController: UIViewController {
     
+    var friends: [Friend] = [
+        Friend(name: "Jimmy", hometown: "LA", hobbies: ["Basketball", "Frisbee", "Golf"])
+    ]
+    
     @IBOutlet weak var tableView: UITableView!
         
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 
+        if segue.identifier == "AddFriendModalSegue" {
+            if let addVC = segue.destination as? AddFriendViewController {
+                addVC.delegate = self
+            }
+        } else if segue.identifier == "ShowFriendDetailSegue" {
+            if let detailVC = segue.destination as? FriendDetailViewController ,
+                let indexPath = tableView.indexPathForSelectedRow {
+                let friend = friends[indexPath.row]
+            
+            
+        
+                detailVC.friend = friend
+            }
+        
+        }
+        
+        
     }
 }
+
 
 extension FriendsTableViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return friends.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "FriendCell", for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "FriendCell", for: indexPath) as? FriendTableViewCell else { return UITableViewCell() }
+        
+        let friend = friends[indexPath.row]
+        cell.friend = friend
         return cell
     }
 }
 
+// delegate
+// step 4 adopt and conform to the delegate, lines 51 - 54
+// step 5 set self as delegate when delegator is created, line 23
+
+extension FriendsTableViewController: AddFriendDelegate {
+    func newFriendAdded(friend: Friend) {
+        friends.append(friend)
+        tableView.reloadData()
+   
+    
+    }
+    
+}
+
+// add vc  as a a data source in main story   drag control from table to VC
